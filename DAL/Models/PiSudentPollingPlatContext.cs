f5ee4ce85eccb1d443a6d52f09a0eda8528d9a7c
ZvonimirIvancic <102034905+ZvonimirIvancic@users.noreply.github.com>
2025-01-02 19:07:52 +0100

    Add files via upload

--- DAL/Models/PiSudentPollingPlatContext.cs
+++ DAL/Models/PiSudentPollingPlatContext.cs
@@ -30,8 +30,7 @@ public partial class PiSudentPollingPlatContext : DbContext
     public virtual DbSet<UserAnswer> UserAnswers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=piSudentPollingPlat;User=sa;Password=SQL;TrustServerCertificate=True;MultipleActiveResultSets=true");
+        => optionsBuilder.UseSqlServer("name=ConnectionStrings:AppConnStr");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
